5cd4f980af048d13db75ab1732594f4b474a69f3
yudhisthereal <yudhisthereal@gmail.com>
2025-01-07 13:16:52 +0700

    initial UI and sqlite setup

---
+++ lib/data/local/database_helper.dart
@@ -0,0 +1,43 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'task_manager.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    // Users Table
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
+        password TEXT
+      )
+    ''');
+
+    // Tasks Table
+    await db.execute('''
+      CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        description TEXT,
+        isCompleted INTEGER,
+        userId INTEGER
+      )
+    ''');
+  }
+}
